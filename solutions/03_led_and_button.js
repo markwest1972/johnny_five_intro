7f672d25c37327cb203f88f740f218954377044c
Mark West <markwest1972@users.noreply.github.com>
2015-04-11 07:43:28 +0200

    Update 03_led_and_button.js

--- solutions/03_led_and_button.js
+++ solutions/03_led_and_button.js
@@ -5,9 +5,9 @@ myBoard = new five.Board();
 
 myBoard.on("ready", function() {
 
-  myLed = new five.Led(11);
+  myLed = new five.Led(4);
 
-  myButton = new five.Button(4)
+  myButton = new five.Button(11)
 
   myButton.on("up", function(value){
     myLed.fadeOut();
